c908fcdd492e184a8c924f8281dd643b068c157d
datgiaosu2k1 <datgiaosu2k1@gmail.com>
2025-08-05 02:44:50 +0900

    Upload BTVN Session 13

---
+++ Luyện tập thao tác với mảng.js
@@ -0,0 +1,13 @@
+let animeDoraemon = [
+  "Doraemon",
+  "Nobita",
+  "Dekisugi",
+  "Shizuka",
+  "Jaian",
+  "Suneo",
+  "Dorami"
+];
+
+for (let i = 0; i < animeDoraemon.length; i++) {
+  console.log(animeDoraemon[i]);
+}
